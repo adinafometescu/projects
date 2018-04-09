294f662f68a69661786b28a3164336efaa221060
adinafometescu <adina.fometescu@gmail.com>
2018-04-09 19:28:21 +0300

    added new module for login

--- beer-your-way/beer-application/src/main/java/beer/way/application/Application.java
+++ beer-your-way/beer-application/src/main/java/beer/way/application/Application.java
@@ -1,9 +1,12 @@
 package beer.way.application;
 
+import login.security.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebSecurityConfig.class)
 public class Application {
 
     public static void main(String[] args) {
