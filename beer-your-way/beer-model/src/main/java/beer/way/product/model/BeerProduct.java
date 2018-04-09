6d4ee7d535d8a3f0b1cd3ea275fce5df6254d275
adinafometescu <adina.fometescu@gmail.com>
2018-04-09 18:27:28 +0300

    added product and beer model

---
+++ beer-your-way/beer-model/src/main/java/beer/way/product/model/BeerProduct.java
@@ -0,0 +1,7 @@
+package beer.way.product.model;
+
+import beer.way.product.Product;
+
+public class BeerProduct extends Product {
+
+}
---
+++ beer-your-way/product-model/src/main/java/beer/way/product/Product.java
@@ -0,0 +1,78 @@
+package beer.way.product;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.Map;
+
+public class Product {
+
+    private String id;
+
+    private Map<Locale, String> title;
+
+    private Map<Locale,String> shortDescription;
+
+    private Map<Locale,String> description;
+
+    private BigDecimal price;
+
+    private int stock;
+
+    private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Map<Locale, String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(Map<Locale, String> title) {
+        this.title = title;
+    }
+
+    public Map<Locale, String> getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(Map<Locale, String> shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public Map<Locale, String> getDescription() {
+        return description;
+    }
+
+    public void setDescription(Map<Locale, String> description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
