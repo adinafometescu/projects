789e91ad9fe41e56fdbe8622080a9c7ef9e6fc10
Adina Fometescu <adina.fometescu@elan-solutions.eu>
2018-04-13 12:28:43 +0300

    added support for i18n messages

--- beer-your-way/beer-application/src/main/java/beer/way/application/Application.java
+++ beer-your-way/beer-application/src/main/java/beer/way/application/Application.java
@@ -1,13 +1,14 @@
 package beer.way.application;
 
-import login.LoginConfiguration;
+import beer.way.application.config.LocaleConfig;
 import beer.way.application.config.WebSecurityConfig;
+import login.LoginConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebSecurityConfig.class, LoginConfiguration.class})
+@Import({WebSecurityConfig.class, LoginConfiguration.class, LocaleConfig.class})
 public class Application {
 
     public static void main(String[] args) {
